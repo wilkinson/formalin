893ee3e5e4575be913726965e454f69d1067c188
Sean Wilkinson <sean@mathbiol.org>
2012-08-07 17:34:44 -0500

    Began organizing data by "type" (for example, binary, ordinal, ...)

--- main.js
+++ main.js
@@ -19,12 +19,38 @@
 
  // Declarations
 
-    var $, conjoin, create_checkbox, Cycle, cycle, generate_report,
+    var $, binary, conjoin, Cycle, cycle, generate_report, ordinal,
         peripheral_smear, trim;
 
  // Definitions
 
     $ = window.jQuery;
+
+    binary = function (obj) {
+     // This function needs documentation.
+        if ((obj instanceof Object) === false) {
+            throw new TypeError('Argument must be an object.');
+        }
+        if (obj.hasOwnProperty('short_name') === false) {
+            throw new Error('Argument must have a `short_name` property.');
+        }
+        obj.states = [undefined, ''];
+        var key = obj.short_name.split(/\s/).join('-').toLowerCase();
+        return $('<input/>', {
+            id: key,
+            click: function () {
+             // This function needs documentation.
+                if ($(this).data('cycle-instance').next().current > 1) {
+                    this.checked = true;
+                }
+                generate_report();
+                return;
+            },
+            type: 'checkbox'
+        }).
+            data('cycle-instance', cycle(obj)).
+            after('<label for="' + key + '">' + obj.short_name + '</label>');
+    };
 
     conjoin = function (x) {
      // This function joins the elements of an array using the "Oxford comma".
@@ -38,26 +64,6 @@
             return ' ' + x.join(' and ') + '.';
         }
         return ' ' + x.slice(0, -1).join(', ') + ', and ' + x.slice(-1) + '.';
-    };
-
-    create_checkbox = function (options) {
-     // This function returns an HTML string for use with jQuery.
-        if ((options instanceof Object) === false) {
-            throw new TypeError('`create_checkbox` expects an object');
-        }
-        if (options.hasOwnProperty('id') === false) {
-            throw new Error('No "id" property specified.');
-        }
-        if (options.hasOwnProperty('label') === false) {
-            throw new Error('No "label" property specified.');
-        }
-        if (options.id.charAt(0) === '#') {
-            options.id = options.id.slice(1);
-        }
-        return [
-            '<input type="checkbox" id="', options.id, '"/>',
-            '<label for="', options.id, '">', options.label, '</label>'
-        ].join('');
     };
 
     Cycle = function Cycle(obj) {
@@ -97,143 +103,49 @@
         return new Cycle(obj);
     };
 
-    generate_report = function (options) {
+    generate_report = function () {
      // This function joins the output from each section's own generating
      // function as text and puts that text into the designated textarea.
-        if ((options instanceof Object) === false) {
-            options = {};
-        }
         $('#report').val([
             peripheral_smear()
         ].join(''));
         return;
     };
 
+    ordinal = function (obj) {
+     // This function needs documentation.
+        if ((obj instanceof Object) === false) {
+            throw new TypeError('Argument must be an object.');
+        }
+        if (obj.hasOwnProperty('short_name') === false) {
+            throw new Error('Argument must have a `short_name` property.');
+        }
+        var key = obj.short_name.split(/\s/).join('-').toLowerCase();
+        return $('<input/>', {
+            id: key,
+            click: function () {
+             // This function needs documentation.
+                if ($(this).data('cycle-instance').next().current > 1) {
+                    this.checked = true;
+                }
+                generate_report();
+                return;
+            },
+            type: 'checkbox'
+        }).
+            data('cycle-instance', cycle(obj)).
+            after('<label for="' + key + '">' + obj.short_name + '</label>');
+    };
+
     peripheral_smear = function () {
      // This function generates the report's "Peripheral Smear" section.
-     //
-     // Example output:
-     //
-     //     Red blood cells [are _ and] show [modifier *cytosis].
-     //     [[modifier *cytes]+ are present].
-     //
-     //     (stippling should be an entirely separate sentence.)
-     //
-        var callback, key, states, x;
-        callback = function (evt) {
+        var y = [];
+        $('#peripheral-smear input:checked').each(function () {
          // This function needs documentation.
-            var key = '#' + evt.currentTarget.id;
-            x[key].next();
-            if (x[key].current > 1) {
-                this.checked = true;
-            }
-            generate_report();
+            y.push($(this).data('cycle-instance'));
             return;
-        };
-        states = {
-            'cytes': [
-                undefined, 'rare', 'few', 'scattered', 'occasional', 'several'
-            ],
-            'cytoses': [
-                undefined, 'mild', 'moderate', 'severe'
-            ],
-            'on_or_off': [
-                undefined, ''
-            ]
-        };
-        x = {
-         // Naming convention here is "#ps-short_name", where "#" allows it to
-         // be a CSS selector and "ps" acts as a namespace of sorts for the
-         // peripheral smear section.
-            '#ps-apk': cycle({
-                long_name:  'anisopoikilocytosis',
-                short_name: 'APK',
-                states:     states.cytoses
-            }),
-            '#ps-aniso': cycle({
-                long_name:  'anisocytosis',
-                short_name: 'aniso',
-                states:     states.cytoses
-            }),
-            '#ps-poikilo': cycle({
-                long_name:  'poikilocytosis',
-                short_name: 'poikilo',
-                states:     states.cytoses
-            }),
-            '#ps-normocytic': cycle({
-                long_name:  'normocytic',
-                short_name: 'normocytic',
-                states:     states.on_or_off
-            }),
-            '#ps-normochromic': cycle({
-                long_name:  'normochromic',
-                short_name: 'normochromic',
-                states:     states.on_or_off
-            }),
-            '#ps-dacro': cycle({
-                long_name:  'dacrocytes',
-                short_name: 'dacro',
-                states:     states.cytes
-            }),
-            '#ps-ellipto': cycle({
-                long_name:  'elliptocytes',
-                short_name: 'ellipto',
-                states:     states.cytes
-            }),
-            '#ps-schisto': cycle({
-                long_name:  'schistocytes',
-                short_name: 'schisto',
-                states:     states.cytes
-            }),
-            '#ps-target': cycle({
-                long_name:  'target cells',
-                short_name: 'target',
-                states:     states.cytes
-            }),
-            '#ps-polychrom': cycle({
-                long_name:  'polychromatic cells',
-                short_name: 'polychrom',
-                states:     states.cytes
-            }),
-            '#ps-stomato': cycle({
-                long_name:  'stomatocytes',
-                short_name: 'stomato',
-                states:     states.cytes
-            }),
-            '#ps-sphero': cycle({
-                long_name:  'spherocytes',
-                short_name: 'sphero',
-                states:     states.cytes
-            }),
-            '#ps-echino': cycle({
-                long_name:  'echinocytes',
-                short_name: 'echino',
-                states:     states.cytes
-            })
-        };
-        for (key in x) {
-            if (x.hasOwnProperty(key)) {
-                $('#peripheral-smear').append(create_checkbox({
-                    id: key,
-                    label: x[key].short_name
-                }));
-                $(key).click(callback);
-            }
-        }
-     // Finally, add a period at the end of our template's "sentence".
-        $('#peripheral-smear').append(' .');
-        peripheral_smear = function () {
-         // THIS FUNCTION NEEDS A LOT OF DOCUMENTATION.
-            var key, y;
-            y = [];
-            for (key in x) {
-                if ((x.hasOwnProperty(key)) && ($(key).is(':checked'))) {
-                    y.push(x[key]);
-                }
-            }
-            return 'Red blood cells show' + conjoin(y);
-        };
-        return peripheral_smear();
+        });
+        return 'Red blood cells show' + conjoin(y);
     };
 
     trim = function (x) {
@@ -253,7 +165,7 @@
         if (this.states.length > 0) {
             this.current = ((this.current + 1) % (this.states.length));
         }
-        return;
+        return this;
     };
 
     Cycle.prototype.toJSON = function () {
@@ -262,31 +174,104 @@
         if (this.states[this.current] !== undefined) {
             return trim(this.states[this.current] + ' ' + this.long_name);
         }
-        return undefined;
+     // When a `toJSON` method returns `undefined`, as this one will, the value
+     // of interest will be excluded by `JSON.stringify`.
+        return;
     };
 
-    Cycle.prototype.toString = function () {
+    Cycle.prototype.toString = Cycle.prototype.valueOf = function () {
      // This function may behave strangely if external code modifies the value
      // of `this.current` directly ...
         if (this.states[this.current] !== undefined) {
             return trim(this.states[this.current] + ' ' + this.long_name);
         }
-        return '(?)';
+        return '';
     };
 
-    Cycle.prototype.valueOf = function () {
-     // This is here for consistency's sake, but I highly recommend against
-     // any code that actually uses it. Since the expected behavior here would
-     // always result in a coercion to `NaN`, I have taken the liberty to
-     // inline that value. Uncomment what follows to see for yourself :-)
-        return this.toString();
-    };
-
- // Out-of-scope definitions (via "invocations" :-P)
+ // Out-of-scope definition by way of an invocation :-)
 
     $(document).ready(function () {
      // This function runs when jQuery decides the page is ready.
-        generate_report({init: true});
+        var states;
+        states = {
+            'cytes': [
+                undefined, 'rare', 'few', 'scattered', 'occasional', 'several'
+            ],
+            'cytoses': [
+                undefined, 'mild', 'moderate', 'severe'
+            ]
+        };
+        $.fn.append.apply($('#peripheral-smear'), [
+            //'Red blood cells are',
+            binary({
+                long_name:  'normocytic',
+                short_name: 'normocytic'
+            }),
+            binary({
+                long_name:  'normochromic',
+                short_name: 'normochromic'
+            }),
+            //' and show',
+            ordinal({
+                long_name:  'anisopoikilocytosis',
+                short_name: 'APK',
+                states:     states.cytoses
+            }),
+            ordinal({
+                long_name:  'anisocytosis',
+                short_name: 'aniso',
+                states:     states.cytoses
+            }),
+            ordinal({
+                long_name:  'poikilocytosis',
+                short_name: 'poikilo',
+                states:     states.cytoses
+            }),
+            ordinal({
+                long_name:  'dacrocytes',
+                short_name: 'dacro',
+                states:     states.cytes
+            }),
+            ordinal({
+                long_name:  'echinocytes',
+                short_name: 'echino',
+                states:     states.cytes
+            }),
+            ordinal({
+                long_name:  'elliptocytes',
+                short_name: 'ellipto',
+                states:     states.cytes
+            }),
+            ordinal({
+                long_name:  'polychromatic cells',
+                short_name: 'polychrom',
+                states:     states.cytes
+            }),
+            ordinal({
+                long_name:  'schistocytes',
+                short_name: 'schisto',
+                states:     states.cytes
+            }),
+            ordinal({
+                long_name:  'spherocytes',
+                short_name: 'sphero',
+                states:     states.cytes
+            }),
+            ordinal({
+                long_name:  'stomatocytes',
+                short_name: 'stomato',
+                states:     states.cytes
+            }),
+            ordinal({
+                long_name:  'target cells',
+                short_name: 'target',
+                states:     states.cytes
+            }),
+         // Add a period at the end of template's "sentence" ;-)
+            ' .'
+        ]);
+     // Finally, initialize the default value of the report.
+        generate_report();
         return;
     });
 
