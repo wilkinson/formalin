a1e486aa2e82f474c8aa75970879cb156f58fa3a
Sean Wilkinson <sean@mathbiol.org>
2013-05-17 15:44:58 -0500

    Linking to local template by default

--- main.js
+++ main.js
@@ -426,7 +426,7 @@
          // "templates" branch as a default.
             location.replace(location.href +
                     ((location.href.slice(-1) === '?') ? '' : '?') +
-                    'http://benchill.github.io/hpath/plugins/default.js');
+                    '//wilkinson.github.io/hpath/plugins/default.js');
             return;
         }
         $.ajaxSetup({cache: true});
