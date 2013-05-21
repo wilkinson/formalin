c23877ec69e8a938531a5c8370bedea71d679e8b
Sean Wilkinson <sean@mathbiol.org>
2013-05-20 20:16:31 -0500

    Fixed HTML generation from JS

--- js/main.js
+++ js/main.js
@@ -195,7 +195,7 @@
             throw new TypeError('Argument must be an object.');
         }
         var key = uuid();
-        return $('<input/>', {
+        return $('<input>', {
             id: key,
             click: function () {
              // This function needs documentation.
@@ -488,12 +488,12 @@
                     }
                     return;
                 });
-                $('#report-output').blur();
                 if (args.length === 0) {
                  // Finally, when all scripts have loaded, we assume that all
                  // sections have been created and insert a horizontal line
                  // before each section before generating the initial report.
-                    $('.section').before('<hr/>');
+                    $('.section').before('<hr>');
+                    $('#report-output').blur();
                     generate_report();
                 } else {
                     window.setTimeout(script_loader, 0, args);
